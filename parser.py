05663e64aaeed91f26bdf8479f3a1ae925a75cbc
jake612 <jake612@live.unc.edu>
2019-03-29 20:23:56 -0400

    List of only relevant attributes l

--- parser.py
+++ parser.py
@@ -1,31 +1,31 @@
 import json
-# Takes in a string from a file of JSON objects and returns list of jsonDict
-def fileStrToJSONObjects(strPassed):
-    jsonStrings = []
-    strLen = len(strPassed)
-    x = 0
-    lastChar = ''
-    newJSON = ''
-    while x < strLen:
-        if lastChar == '}' and strPassed[x] == '{':
-            jsonStrings.append(newJSON)
-            newJSON = ''
-        else:
-            lastChar = strPassed[x]
-            newJSON += strPassed[x]
-        x += 1
-
-    jsonObj = []
-    for j in jsonStrings:
-        jsonObj.append(json.loads(j))
-
-    return jsonObj
-
+import matplotlib.pyplot as plt
+# Takes in a file object and returns a list containing a dictionary for each JSON
 def parseByLine(fileObj):
     jsonDict = []
     for i in fileObj:
-        jsonDict.append(json.loads(i))
+        jsonDict.append(extractAttributes(json.loads(i)))
     return jsonDict
+
+# Takes in a JSON Tweet dictionary and returns dictionary of pertinent
+def extractAttributes(dictObj):
+    newDict = {}
+    newDict['id'] = dictObj.get('id')
+    newDict['created_at'] = dictObj.get('created_at')
+    newDict['in_reply_to_status_id'] = dictObj.get('in_reply_to_status_id')
+    newDict['retweet_count'] = dictObj.get('retweet_count')
+    newDict['lang'] = dictObj.get('lang')
+    newDict['hashtags'] = len(dictObj.get('entities').get('hashtags'))
+    newDict['urls'] = len(dictObj.get('entities').get('urls'))
+    newDict['user_mentions'] = len(dictObj.get('entities').get('user_mentions'))
+    newDict['followers_count'] = dictObj.get('user').get('followers_count')
+    newDict['friends_count'] = dictObj.get('user').get('friends_count')
+    newDict['listed_count'] = dictObj.get('user').get('listed_count')
+    newDict['verified'] = dictObj.get('user').get('verified')
+    newDict['statuses_count'] = dictObj.get('user').get('statuses_count')
+    newDict['time_zone'] = dictObj.get('user').get('time_zone')
+    return newDict
+
 
 # opening a file to string fo
 filename = input("Enter a file name to parse\n")
@@ -33,4 +33,10 @@ fo = open(filename, "r")
 
 # create a dictionary from JSON file
 jsonDict = parseByLine(fo)
+fo.close()
 
+i = 0
+while i < 100:
+    print(jsonDict.pop(i)['friends_count'])
+
+    i += 1
